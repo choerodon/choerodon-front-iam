7a3e5b5b82e8bf4d3b513ecb4e8296595c865c80
陈彬杰 <binjie.chen@hand-china.com>
2018-10-31 17:45:49 +0800

    [IMP] 新手指引优化

--- iam/src/app/iam/guide/SiteBasic/index.js
+++ iam/src/app/iam/guide/SiteBasic/index.js
@@ -7,7 +7,6 @@ import { injectIntl } from 'react-intl';
 import { StepFooter, StepBar, GuideMask, AutoGuide } from 'choerodon-front-boot';
 import EasyImg from '../../components/easyImg';
 import picCreatOrg from './image/create-org.png';
-import picAddPer from './image/add-permission.png';
 import picSiteRole from './image/site-role.png';
 import picRootRole from './image/root-role.png';
 import picAddMenu from './image/add-menu.png';
@@ -17,6 +16,17 @@ import picCard from './image/card.png';
 @injectIntl
 @observer
 export default class SiteBasic extends Component {
+  getGuideCard(iconType, name) {
+    return (
+      <div className="guide-card">
+        <Icon type={iconType} style={{ color: 'rgba(0,0,0,0.65)', fontSize: '24px' }} />
+        <span style={{ color: '#000', fontSize: '16px' }}>{name}</span>
+      </div>
+    );
+  }
+
+  handleCheck = dom => dom.children[0].className !== 'guide-card'
+
   renderStep(current) {
     const { AppState, GuideStore, intl } = this.props;
     switch (current) {
@@ -43,36 +53,35 @@ export default class SiteBasic extends Component {
           <div>
             <h1>{intl.formatMessage({ id: 'guide.iam.sitebasic.h1' })}</h1>
             <h2>导航至平台设置页面</h2>
-            <Timeline>
+            <Timeline style={{ height: '100px' }}>
               <Timeline.Item>
                 <p>
-                点击<GuideMask highLight="icon-settings" level={1}>管理
-                  <div className="inline-icon-setting" /></GuideMask>按钮
+                点击<GuideMask highLight="icon-settings" level={1}>管理</GuideMask>按钮
                 </p>
               </Timeline.Item>
               <Timeline.Item>
                 <p>
-                打开平台左侧的<GuideMask highLight="c7n-boot-header-logo-menu-icon" level={0}>菜单
-                  <div className="inline-main-menu" /></GuideMask>
+                打开平台左侧的<GuideMask highLight="c7n-boot-header-logo-menu-icon" level={0}>菜单</GuideMask>
                 </p>
               </Timeline.Item>
               <Timeline.Item>
                 <p>选择<GuideMask highLight="icon-IAM" level={2}>平台设置</GuideMask>部分</p>
-                <AutoGuide
-                  highLight={['icon-settings', 'icon-settings', 'c7n-boot-header-logo-menu-icon', 'c7n-boot-header-logo-menu-icon', 'icon-IAM']}
-                  idx={[0, 0, 0, 0, 0]}
-                  level={[1, 1, 0, 0, 2]}
-                  mode={['mask', 'click', 'mask', 'click', 'mask']}
-                  onStart={() => AppState.setMenuExpanded(false)}
-                >
-                  <div className="menubox">
-                    <div className="show-menu-img-site-setting show-menu-img" />
-                    <Icon className="showicon" type="play_circle_filled" />
-                    <span>互动演示</span>
-                  </div>
-                </AutoGuide>
               </Timeline.Item>
-            </Timeline> 
+            </Timeline>
+            <AutoGuide
+              highLight={['icon-settings', 'icon-settings', 'c7n-boot-header-logo-menu-icon', 'c7n-boot-header-logo-menu-icon', 'icon-IAM']}
+              idx={[0, 0, 0, 0, 0]}
+              level={[1, 1, 0, 0, 2]}
+              mode={['mask', 'click', 'mask', 'click', 'mask']}
+              onStart={() => AppState.setMenuExpanded(false)}
+            >
+              <div className="guide-card">
+                <Icon type="IAM" style={{ color: 'rgba(0,0,0,0.65)', fontSize: '24px' }} />
+                <span style={{ color: '#000', fontSize: '16px' }}>平台设置</span>
+                <Icon type="play_circle_filled" style={{ marginLeft: 'calc(100% - 199.5px)', fontSize: '15px' }} />
+                <span style={{ marginRight: '16px' }}>互动演示</span>
+              </div>
+            </AutoGuide>
           </div>
         );
       
@@ -84,8 +93,10 @@ export default class SiteBasic extends Component {
             <p>组织是项目的上一个层级，用户必须属于一个组织。通过组织管理您可以创建、启停用组织。</p>
             <Timeline>
               <Timeline.Item>
-                <p>选择<GuideMask highLight="icon icon-manage_organization" level={2}>组织管理</GuideMask></p>
-                <GuideMask highLight="icon icon-manage_organization" className="no-border" level={2}><div className="menu-img-org-man menu-img" /></GuideMask>
+                <p>选择<GuideMask highLight="icon icon-manage_organization" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>组织管理</GuideMask></p>
+                <GuideMask highLight="icon icon-manage_organization" className="no-border" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>
+                  {this.getGuideCard('manage_organization', '组织管理')}
+                </GuideMask>
               </Timeline.Item>
               <Timeline.Item>
                 <p>点击 <GuideMask highLight="icon-playlist_add" level={1} siteLevel="site" route="/iam/organization">创建组织</GuideMask>按钮。</p>
@@ -97,7 +108,7 @@ export default class SiteBasic extends Component {
               </Timeline.Item>
             </Timeline>
             <div />
-            <p><span className="tip">注意:</span>创建组织后，平台默认您为此组织的组织管理员。</p>
+            <p><Icon type="info" style={{ color: '#ffb100', marginBottom: '3px' }} /><span> 创建组织后，平台默认您为此组织的组织管理员。</span></p>
           </div>
         );
       
@@ -109,22 +120,23 @@ export default class SiteBasic extends Component {
             <p>角色是您可分配给成员的一组权限。通过角色管理您可以创建、启停用角色，为角色添加权限。</p>
             <Timeline>
               <Timeline.Item>
-                <p>选择<GuideMask highLight="icon-assignment_ind" level={2}>角色管理</GuideMask></p>
-                <GuideMask highLight="icon-assignment_ind" className="no-border" level={2}><div className="menu-img-role-man menu-img" /></GuideMask>
+                <p>选择<GuideMask highLight="icon-assignment_ind" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>角色管理</GuideMask></p>
+                <GuideMask highLight="icon-assignment_ind" className="no-border" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>
+                  {this.getGuideCard('assignment_ind', '角色管理')}
+                </GuideMask>
               </Timeline.Item>
               <Timeline.Item>
                 <p>点击 <GuideMask highLight="icon-playlist_add" level={1} siteLevel="site" route="/iam/role">创建角色</GuideMask>按钮。</p>
                 <ul className="ul1">
                   <li>选择角色层级，输入角色编码、角色名称。</li>
                   <li>选择角色标签。（选填）</li>
-                  <li>点击添加权限
-                    <div className="inline-add-role" />,选择要给角色添加的权限.</li>
-                  <EasyImg src={picAddPer} />
+                  <li>点击
+                    <GuideMask highLight="icon-add" level={1} siteLevel="site" route="/iam/role/create">添加权限</GuideMask>,选择要给角色添加的权限.</li>
                   <li>点击创建按钮完成角色创建</li>
                 </ul>
               </Timeline.Item>
             </Timeline>
-            <p><span className="tip">注意:</span>角色标签用于定义角色的特定逻辑的功能，需与代码开发结合。</p>
+            <p><Icon type="info" style={{ color: '#ffb100', marginBottom: '3px' }} /><span> 角色标签用于定义角色的特定逻辑的功能，需与代码开发结合。</span></p>
           </div>
         );
       case 4:
@@ -135,8 +147,10 @@ export default class SiteBasic extends Component {
             <p>通过平台角色分配您可以向成员分配平台层的角色，以便于成员有权限在平台层操作。</p>
             <Timeline>
               <Timeline.Item>
-                <p>选择<GuideMask highLight="icon-person_add" level={2}>平台角色分配</GuideMask></p>
-                <GuideMask highLight="icon-person_add" level={2} siteLevel="site" className="no-border"><div className="menu-img-site-role-ass menu-img" /></GuideMask>
+                <p>选择<GuideMask highLight="icon-person_add" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>平台角色分配</GuideMask></p>
+                <GuideMask highLight="icon-person_add" level={2} siteLevel="site" className="no-border" mode="checkMask" onCheck={dom => this.handleCheck(dom)}>
+                  {this.getGuideCard('person_add', '平台角色分配')}
+                </GuideMask>
               </Timeline.Item>
               <Timeline.Item>
                 <p>点击 <GuideMask highLight="icon-playlist_add" level={1} siteLevel="site" route="/iam/member-role">添加</GuideMask>按钮。 </p>
@@ -159,8 +173,8 @@ export default class SiteBasic extends Component {
             <p>Root用户拥有系统的最高权限。他可以管理平台以及平台上的所有组织和项目。通过Root用户设置您可以添加或移除root用户。</p>
             <Timeline>
               <Timeline.Item>
-                <p>选择<GuideMask highLight="icon-root" level={2} siteLevel="site">Root用户设置</GuideMask></p>
-                <div className="menu-img-root-setting menu-img" />
+                <p>选择<GuideMask highLight="icon-root" level={2} siteLevel="site" mode="checkMask" onCheck={dom => this.handleCheck(dom)}>Root用户设置</GuideMask></p>
+                <GuideMask highLight="icon-root" level={2} siteLevel="site" className="no-border" mode="checkMask" onCheck={dom => this.handleCheck(dom)}>{this.getGuideCard('root', 'Root用户设置')}</GuideMask>
               </Timeline.Item>
               <Timeline.Item>
                 <p>点击 <GuideMask highLight="icon-playlist_add" level={1} siteLevel="site" route="/iam/root-user">添加</GuideMask>按钮。</p>
@@ -171,7 +185,7 @@ export default class SiteBasic extends Component {
                 <EasyImg src={picRootRole} />
               </Timeline.Item>
             </Timeline>
-            <p><span className="tip">注意:</span>Root用户拥有系统的最高权限，请谨慎操作。</p>
+            <p><Icon type="info" style={{ color: '#f44336', marginBottom: '3px' }} /><span> Root用户拥有系统的最高权限，请谨慎操作。</span></p>
           </div>
         );
       case 6:
@@ -182,23 +196,43 @@ export default class SiteBasic extends Component {
             <p>您可以自定义您的菜单，仪表盘以及平台logo等标志性配置。</p>
             <Timeline>
               <Timeline.Item>
-                <p>选择<GuideMask highLight="icon-view_list" level={2}>菜单配置</GuideMask></p>
-                <GuideMask highLight="icon-view_list" level={2} className="no-border"><div className="menu-img-menu-setting menu-img" /></GuideMask>
+                <p>选择<GuideMask highLight="icon-view_list" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>菜单配置</GuideMask></p>
+                <GuideMask highLight="icon-view_list" level={2} className="no-border" mode="checkMask" onCheck={dom => this.handleCheck(dom)}>
+                  {this.getGuideCard('view_list', '菜单配置')}
+                </GuideMask>
                 <p>拖动目录/菜单，调整目录/菜单的顺序。</p>
                 <p>点击 <GuideMask highLight="icon-playlist_add" level={1} siteLevel="site" route="/iam/menu-setting">创建目录</GuideMask>按钮。向对应层级添加目录。</p>
                 <EasyImg src={picAddMenu} />
                 <p>点击<GuideMask highLight="ant-btn-primary" level={0} siteLevel="site" route="/iam/menu-setting">保存</GuideMask>按钮完成菜单配置。</p>
               </Timeline.Item>
               <Timeline.Item>
-                <p>选择<GuideMask highLight="icon-donut_small" level={2}>仪表盘配置</GuideMask></p>
-                <GuideMask highLight="icon-donut_small" level={2} className="no-border"><div className="menu-img-menu-setting menu-img" /></GuideMask>
-                <p>点击<GuideMask highLight="icon-mode_edit" level={1} siteLevel="site" route="/iam/dashboard-setting">笔<div className="inline-pen" /></GuideMask>图标，您可以修改卡片的信息，选择是否开启角色控制。</p>
+                <p>
+                  选择
+                  <GuideMask highLight="icon-donut_small" level={2} mode="checkMask" onCheck={dom => this.handleCheck(dom)}>仪表盘配置</GuideMask>
+                </p>
+                <GuideMask highLight="icon-donut_small" level={2} className="no-border" mode="checkMask" onCheck={dom => this.handleCheck(dom)}>
+                  {this.getGuideCard('donut_small', '仪表盘配置')}
+                </GuideMask>
+                <p>
+                  点击
+                  <GuideMask highLight="icon-mode_edit" level={1} siteLevel="site" route="/iam/dashboard-setting">
+                    笔
+                    <Icon type="mode_edit" style={{ fontSize: 10 }} />
+                  </GuideMask>图标，您可以修改卡片的信息，选择是否开启角色控制。
+                </p>
                 <EasyImg src={picCard} />
-                <p>点击<GuideMask highLight="icon-finished" level={1} siteLevel="site" route="/iam/dashboard-setting">启停用<div className="inline-stop" /></GuideMask>按钮，可以控制此卡片是否启停用。</p>
+                <p>点击
+                  <GuideMask highLight="icon-finished" level={1} siteLevel="site" route="/iam/dashboard-setting">
+                    启停用
+                    <Icon type="finished" style={{ fontSize: 10 }} />
+                  </GuideMask>按钮，可以控制此卡片是否启停用。
+                </p>
               </Timeline.Item>
               <Timeline.Item>
                 <p>选择<GuideMask highLight="icon-settings" idx={1} level={2}>系统配置</GuideMask></p>
-                <GuideMask highLight="icon-settings" idx={1} level={2} className="no-border"><div className="menu-img-settings menu-img" /></GuideMask>
+                <GuideMask highLight="icon-settings" idx={1} level={2} className="no-border">
+                  {this.getGuideCard('settings', '系统配置')}
+                </GuideMask>
                 <p>您可以上传平台徽标、图形标，自定义平台简称、全称，更改平台默认密码和默认语言的顺序。</p>
                 <p>点击<GuideMask highLight="ant-btn-primary" level={0} siteLevel="site" route="/iam/system-setting">保存</GuideMask>按钮完成系统配置。</p>
                 <p>您可以点击 <GuideMask highLight="icon-swap_horiz" level={1} siteLevel="site" route="/iam/system-setting">重置</GuideMask>按钮，还原到默认配置。</p>

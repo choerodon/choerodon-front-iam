41478e40deb62c48bff63e5d589aca87603f8e88
陈彬杰 <binjie.chen@hand-china.com>
2018-11-15 14:36:47 +0800

    [FIX] 修复allowConfig接口权限错误

--- iam/src/app/iam/stores/user/receive-setting/ReceiveSettingStore.js
+++ iam/src/app/iam/stores/user/receive-setting/ReceiveSettingStore.js
@@ -89,7 +89,7 @@ class ReceiveSettingStore {
   }
 
   @action loadAllowConfigData() {
-    return axios.get('notify/v1/notices/send_settings/list').then(action(
+    return axios.get('notify/v1/notices/send_settings/list/allow_config').then(action(
       (data) => {
         if (data.failed) {
           Choerodon.prompt(data.message);

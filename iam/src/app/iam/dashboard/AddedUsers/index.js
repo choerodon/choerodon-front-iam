3a750ee5800bc5423bbe742c841f58f9c8466115
hulingfangzi <hulingfangzi@163.com>
2018-12-26 11:34:04 +0800

    [ADD]添加新增用户仪表盘配置

---
+++ iam/src/app/iam/dashboard/AddedUsers/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import './index.scss';
+
+export default class AddedUsers extends Component {
+  render() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+}
